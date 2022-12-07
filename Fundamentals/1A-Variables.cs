8ead680718ea1a18890bfb06f22de3c6ef52295d
Bishnu Rawal <rawal.bishnu@gmail.com>
2022-12-07 08:06:46 +0545

    Learn about array and more on methods

--- Fundamentals/1A-Variables.cs
+++ Fundamentals/1A-Variables.cs
@@ -30,6 +30,44 @@ class Variable
         DateTime dob;
         DateOnly chunabDay;
         TimeOnly meetingToday;
-        
+
+        // Arrays
+        // One dimensional
+        byte[] personAges = new byte[5];
+        personAges[0] = 89;
+        personAges[1] = 9;
+        personAges[2] = 18;
+        personAges[3] = 67;
+        personAges[4] = 23;
+        var x = personAges[2];
+
+        byte[] peopleAges = { 89, 9, 18, 67, 23 };
+
+        // Write an array to hold student names
+        string[] names = { "Ram", "Shyam", "Lakhan", "Pranish" };
+
+        // Multi dimensinal
+
+        //  3   8   9
+        //  8   7   3
+
+        int[,] mat = new int[2, 3];
+        mat[0, 0] = 3;
+        mat[0, 1] = 8;
+        mat[0, 2] = 9;
+        mat[1, 0] = 8;
+        mat[1, 1] = 7;
+        mat[1, 2] = 3;
+
+        int[,] table = { { 3, 8, 9, 5 }, { 8, 7, 3, 7 } };
+
+        // Jagged array
+        //  2   4   7
+        //  1   5
+        //  9   8   4   5
+        short[][] items = new short[3][];
+        items[0] = new short[] {2, 4, 7};
+        items[1] = new short[] {1, 5};
+        items[2] = new short[] {9, 8, 4, 5};
     }
 }
--- Fundamentals/EntryPoint.cs
+++ Fundamentals/EntryPoint.cs
@@ -3,62 +3,44 @@
     public static void Main()
     {
         // Console.Write("Enter your name:");
-        // string x = Console.ReadLine();
+        // var x = Console.ReadLine();
         // Console.WriteLine(x + " Wow, Nice name");
         // Console.WriteLine("Nepal is beautiful country");
 
-        Selection sel1 = new(); 
-        sel1.LearnSelectionStatements();
+        // Selection sel1 = new(); 
+        // sel1.LearnSelectionStatements();
 
-        //Loops, iterations
-        // for, while, do-while, foreach
+        Loop l = new();
+        Loop l1 = new();
 
-        for (int counter = 60; counter > 50; counter--)
-        {
-            Console.WriteLine("Nepal");
-        }
+        l.LearnLoops();
+        l1.LearnLoops();
 
-        for (byte num = 1; num <= 100; num++)
-        {
-            Console.Write(num + " ");
-        }
+        Method m = new();
+        m.Print();
+        m.Print("Nepal");
+        var mess = m.GetMessage();
+        var sum = m.Add(23.5, 56.8);
 
-        int n = 1;
-        while(n <= 100)
-        {
-            Console.Write(n + " ");
-            n++;
-        }
+        var a = m.FindAverage(2345, 4324, 3535);
+        var b = m.FindAverage(2345, 4324, 3535, 79238427);
+        var c = m.FindAverage(3, 4, 5, 5, 2345, 4324, 3535, 79238427);
 
-        // While - use case
-        string key = "y";
-        while(key == "y")
-        {
-            Console.WriteLine("Nepal");
 
-            key = Console.ReadLine();
-        }
+        // Type casting
+        // byte a = 234;
+        // int b = a;          // Implicit casting
+        // byte c = (byte)b;   // Explicit casting
 
-        // CW - Print even numbers less than 500
-        // CW - Print odd numbers less than 500
-        // CW - Print all numbers less than 500 which are multiple of 3 and 5
-        Console.WriteLine("Odd numbers:");
-        for (int num = 0; num < 500; num++)
-        {
-            if (num % 2 != 0)
-            {
-                Console.Write(num + " ");
-            }
-        }
+        var age = 23;
+        var name = "Ram";
+        var m1 = name + " is " + age + " years old";
+        var mi = $"{name} is {age} years old."; // String interpolation
 
-        Console.WriteLine("Multiples of 3 and 5:");
-        for (int num = 0; num < 500; num++)
-        {
-            if (num % 3 == 0 && num % 5 == 0 )
-            {
-                Console.Write(num + " ");
-            }
-        }
+        Console.WriteLine(mi);
+
+
+
 
 
     }
