d6146a55a3f03b555484fb9ead1673f7302ecbc6
Bishnu Rawal <rawal.bishnu@gmail.com>
2022-12-15 08:04:19 +0545

    More on File IO

--- Fundamentals/1K-FileIO.cs
+++ Fundamentals/1K-FileIO.cs
@@ -18,7 +18,25 @@ class FileIO
 
     public void CreateDirectoriesAndFiles()
     {
-        // CW - Create ten folders named Folder 1, Folder 2 and so on, inside some root folder
-        
+        // CW - Create ten folders named Folder 1, Folder 2 and so on
+        // Each of these folder should contain a file with text "I am in folder n", 
+        // n being the number of folder they are in.
+
+        for (byte counter = 0; counter <= 10; counter++)
+        {
+            var folderName = "Folder" + counter;
+            var folderPath = @$"D:\Apps\.NET Training 5\abc\{folderName}";
+            Directory.CreateDirectory(folderPath);
+
+            var filePath = folderPath + @"\test.cs";
+            File.WriteAllText(filePath, $"// I am in {folderName}");
+        }        
+    }
+
+    public void GetFileMetaData()
+    {
+        FileInfo info = new FileInfo(@"D:\Apps\.NET Training 5\.NET-bishnu\readme.md");
+
+        var size = info.Length / 1024 + " KB";        
     }
 }
