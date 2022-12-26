8cd51ba6fa334ebea048db07ca3d1aa12d78b64a
Bishnu Rawal <rawal.bishnu@gmail.com>
2022-12-26 07:52:44 +0545

    Add multiplication table

--- Web Development/HelloWeb/Controllers/NumberController.cs
+++ Web Development/HelloWeb/Controllers/NumberController.cs
@@ -3,8 +3,18 @@ using Microsoft.AspNetCore.Mvc;
 
 public class NumberController: Controller
 {
-    public IActionResult Greet()       //  /number/greet
+    public IActionResult MTable()       //  /number/greet
     {
         return View();
+    }
+
+    public IActionResult MultiplicationTable(int value)
+    {
+        return View(value);
+    }
+
+    public IActionResult MultiplicationTableFromTo(int from, int to)
+    {        
+        return View((from, to));
     }
 }
