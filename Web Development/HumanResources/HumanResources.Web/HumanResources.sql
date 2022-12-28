73b29e6385e52d527dee81d234b3a4e4d42c2c9f
Bishnu Rawal <rawal.bishnu@gmail.com>
2022-12-28 08:43:32 +0545

    Add HRM app template

---
+++ Web Development/HumanResources/HumanResources.Web/HumanResources.sql
@@ -0,0 +1,25 @@
+﻿
+create table Employee
+(
+	Id int,
+	Name nvarchar(200),
+	Address nvarchar(300),
+	Gender char(1),
+	Dob datetime
+)
+
+drop table Employee
+create table Employee
+(
+	Id int not null identity primary key,
+	Name nvarchar(200) not null,
+	Address nvarchar(300),
+	Gender char(1) not null,
+	Dob datetime
+)
+
+insert into Employee
+values ('Ashesh Shrestha', 'Kathmandu', 'M', '1988/12/12')
+
+select * from Employee
+
