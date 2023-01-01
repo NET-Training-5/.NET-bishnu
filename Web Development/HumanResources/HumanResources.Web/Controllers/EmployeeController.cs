e494f4f34bcddfbf765134cf3bf3d3a9abebdecb
Bishnu Rawal <rawal.bishnu@gmail.com>
2023-01-01 08:09:29 +0545

    Write C of CRUD

--- Web Development/HumanResources/HumanResources.Web/Controllers/EmployeeController.cs
+++ Web Development/HumanResources/HumanResources.Web/Controllers/EmployeeController.cs
@@ -1,14 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HumanResources.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HumanResources.Web.Controllers;
 
 public class EmployeeController : Controller
 {
+    HRDbContext db = new HRDbContext();
+
     public IActionResult Index()
     {
-        HRDbContext db = new HRDbContext();
         var employees = db.Employees.ToList();
 
         return View(employees);
+    }
+
+    public IActionResult Add22()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Add(Employee employee)
+    {
+        db.Employees.Add(employee);
+        db.SaveChanges();
+
+        return RedirectToAction(nameof(Index));
     }
 }
--- Web Development/HumanResources/HumanResources.Web/Models/Employee.cs
+++ Web Development/HumanResources/HumanResources.Web/Models/Employee.cs
@@ -1,4 +1,5 @@
 ﻿using HumanResources.Web.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace HumanResources.Web.Models;
 public class Employee
@@ -6,7 +7,10 @@ public class Employee
     public int Id { get; set; }
     public string Name { get; set; }
     public string? Address { get; set; }
+    
+    [DataType(DataType.EmailAddress)]
     public string Email { get; set; }
+    
     public Gender Gender { get; set; }
     public DateTime? Dob { get; set; }
     public DateTime JoinDate { get; set; }
