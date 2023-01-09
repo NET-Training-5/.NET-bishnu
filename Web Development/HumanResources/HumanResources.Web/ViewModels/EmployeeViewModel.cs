65c6371e72ca13357d2cdfbca10a1df446977b5d
Bishnu Rawal <rawal.bishnu@gmail.com>
2023-01-09 07:29:27 +0545

    Add client side validations

--- Web Development/HumanResources/HumanResources.Web/ViewModels/EmployeeViewModel.cs
+++ Web Development/HumanResources/HumanResources.Web/ViewModels/EmployeeViewModel.cs
@@ -1,25 +1,34 @@
 ﻿using HumanResources.Web.Enums;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HumanResources.Web.ViewModels;
 public class EmployeeViewModel
 {
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "We need your name")]
+    [MinLength(2, ErrorMessage = "Please enter at least 2 letters")]
     public string Name { get; set; }
     public string? Address { get; set; }
     
     [DataType(DataType.EmailAddress)]
+    [Required(ErrorMessage = "We need your email id")]
     public string Email { get; set; }
     
     public Gender Gender { get; set; }
+
+    [Display(Name="Date Of Birth")]
     public DateTime? Dob { get; set; }
+
+    [Display(Name = "Join Date")]
     public DateTime JoinDate { get; set; }
     public string Designation { get; set; }
-        
+
+    [Display(Name = "Upload Your Avatar")]
     public IFormFile ProfileImage { get; set; }
     public string? ProfileImagePath { get; set; }
 
+    [Display(Name = "Department")]
     public string? DepartmentName { get; set; }
     public int DepartmentId { get; set; } 
 }
