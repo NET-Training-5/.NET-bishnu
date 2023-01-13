c3190d79e5eddd91504279b54e7eb0c070dd2468
Bishnu Rawal <rawal.bishnu@gmail.com>
2023-01-13 07:31:10 +0545

    Add get all employees endpoint

---
+++ Web Development/HumanResources/HumanResources.Api/ApiModels/EmployeeApiModel.cs
@@ -0,0 +1,18 @@
+﻿using HumanResources.ApplicationCore.Enums;
+
+namespace HumanResources.Web.ViewModels;
+public class EmployeeApiModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string? Address { get; set; }
+    public string Email { get; set; }    
+    public Gender Gender { get; set; }
+    public DateTime? Dob { get; set; }
+    public DateTime JoinDate { get; set; }
+    public string Designation { get; set; }
+    public IFormFile ProfileImage { get; set; }
+    public string? ProfileImagePath { get; set; }
+    public string? DepartmentName { get; set; }
+    public int DepartmentId { get; set; } 
+}
---
+++ Web Development/HumanResources/HumanResources.Api/Controllers/EmployeesController.cs
@@ -0,0 +1,44 @@
+﻿using HumanResources.Web.Models;
+using HumanResources.Web.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HumanResources.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly HRDbContext dbContext;
+
+        public EmployeesController(HRDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<List<EmployeeApiModel>> GetEmployees()
+        {
+            var employees = from e in dbContext.Employees
+                            join d in dbContext.Departments
+                            on e.DepartmentId equals d.Id
+                            select new EmployeeApiModel
+                            {
+                                Id = e.Id,
+                                Name = e.Name,
+                                Address = e.Address,
+                                Designation = e.Designation,
+                                Dob = e.Dob,
+                                Email = e.Email,
+                                Gender = e.Gender,
+                                JoinDate = e.JoinDate,
+                                ProfileImagePath = e.ProfileImagePath,
+                                DepartmentName = d.Name
+                            };
+
+            //var employees = await dbContext.Employees.Include(e => e.Department).ToListAsync();
+            return await employees.ToListAsync();
+        }
+    }
+}
