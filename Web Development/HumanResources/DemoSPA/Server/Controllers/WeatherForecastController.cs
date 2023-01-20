5e46a2b00fe0917ec6a2d369d267b186e5943485
Bishnu Rawal <rawal.bishnu@gmail.com>
2023-01-20 07:02:46 +0545

    Add SPA template

---
+++ Web Development/HumanResources/DemoSPA/Server/Controllers/WeatherForecastController.cs
@@ -0,0 +1,34 @@
+using DemoSPA.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoSPA.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ForecastsController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<ForecastsController> _logger;
+
+        public ForecastsController(ILogger<ForecastsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 50).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
